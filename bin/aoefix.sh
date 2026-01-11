4339825ea8f5083ec515cf6e2a15fd91d58e709e
Thibault Suzanne <thi.suzanne@gmail.com>
2026-01-11 20:27:39 +0000

    Add script to fix aoe resources

---
+++ bin/aoefix.sh
@@ -0,0 +1,4 @@
+sudo cp ~/.local/share/Steam/steamapps/common/AoE2DE/vc_redist.x64.exe ~/.local/share/Steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32/vc_redist.x64.exe
+cd ~/.local/share/Steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32
+sudo cabextract vc_redist.x64.exe
+sudo cabextract a10
